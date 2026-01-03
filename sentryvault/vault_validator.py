89bf3c7cd1aba94e918a9f407f1bf9b1b952dad0
Hope Mabuza <londiwemabuza94@gmail.com>
2026-01-03 21:35:45 +0200

    added logic for vault_validator

--- sentryvault/vault_validator.py
+++ sentryvault/vault_validator.py
@@ -4,33 +4,53 @@ class Vault_Validator:
         #method for checking if the length of the password is equals to or greater than 8
         #parameter -> password
         #returns -> boolean
-        return True
+
+        return len(password) >= 8
+
 
     def contains_uppercase(self, password: str) -> bool:
         #method for checking if the password contains any uppercase
         #parameter -> password
         #returns -> boolean
-        return True
+        for i in password:
+            if i.isupper():
+                return True
+        return False
+            
 
     def contains_lowercase(self, password: str) -> bool:
         #method for checking if the password contains any lowercase
         #parameter -> password
         #returns -> boolean
-        return True
+        for i in password:
+            if i.islower():
+                return True
+        return False
+
 
     def contains_digit(self, password: str) -> bool:
         #method for checking if the password contains any digits
         #parameter -> password
         #returns -> boolean
-        return True
+        for i in password:
+            if i.isdigit():
+                return True
+        return False
+
 
     def contains_special_character(self, password: str) -> bool:
         #method for checking if the password contains any special character
         #parameter -> password
         #returns -> boolean
-        return True
+        for i in password:
+            if not i.isalnum():
+                return True
+        return False
 
     def is_valid_password(self, password: str) -> bool:
         #checks if all the above methods are true for the password to be valid
-        return True
-
+        return ( self.is_valid_length(password) and  
+                self.contains_uppercase(password) and 
+                self.contains_lowercase(password) and 
+                self.contains_digit(password) and 
+                self.contains_special_character(password) )
