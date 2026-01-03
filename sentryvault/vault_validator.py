69f7830fee6db78b6d1d340644f5ace5327b14ca
Hope Mabuza <londiwemabuza94@gmail.com>
2026-01-03 21:12:37 +0200

    added unittest for vault_validator file

--- sentryvault/vault_validator.py
+++ sentryvault/vault_validator.py
@@ -1,35 +1,36 @@
+class Vault_Validator:
 
-def is_valid_length(password):
-    #method for checking if the length of the password is equals to or greater than 8
-    #parameter -> password
-    #returns -> boolean
-    return True
+    def is_valid_length(self, password: str) -> bool:
+        #method for checking if the length of the password is equals to or greater than 8
+        #parameter -> password
+        #returns -> boolean
+        return True
 
-def contains_uppercase(password):
-    #method for checking if the password contains any uppercase
-    #parameter -> password
-    #returns -> boolean
-    return True
+    def contains_uppercase(self, password: str) -> bool:
+        #method for checking if the password contains any uppercase
+        #parameter -> password
+        #returns -> boolean
+        return True
 
-def contains_lowercase(password):
-    #method for checking if the password contains any lowercase
-    #parameter -> password
-    #returns -> boolean
-    return True
+    def contains_lowercase(self, password: str) -> bool:
+        #method for checking if the password contains any lowercase
+        #parameter -> password
+        #returns -> boolean
+        return True
 
-def contains_digit(password):
-    #method for checking if the password contains any digits
-    #parameter -> password
-    #returns -> boolean
-    return True
+    def contains_digit(self, password: str) -> bool:
+        #method for checking if the password contains any digits
+        #parameter -> password
+        #returns -> boolean
+        return True
 
-def contains_special_character(password):
-    #method for checking if the password contains any special character
-    #parameter -> password
-    #returns -> boolean
-    return True
+    def contains_special_character(self, password: str) -> bool:
+        #method for checking if the password contains any special character
+        #parameter -> password
+        #returns -> boolean
+        return True
 
-def is_valid_password(password):
-    #checks if all the above methods are true for the password to be valid
-    return True
+    def is_valid_password(self, password: str) -> bool:
+        #checks if all the above methods are true for the password to be valid
+        return True
 
--- tests/test_vault_validator.py
+++ tests/test_vault_validator.py
@@ -9,5 +9,47 @@ class TestVaultValidator(unittest.TestCase):
     def test_valid_char_length(self):
         self.assertTrue(self.checker.is_valid_length("njdyu877e7gbhcd"))
 
+    def test_invalid_char_length(self):
+        self.assertFalse(self.checker.is_valid_length("nvbhg"))
+
+
+    def test_contains_uppercase(self):
+        self.assertTrue(self.checker.contains_uppercase("shjhgSGJG"))
+
+    def test_no_uppercase(self):
+        self.assertFalse(self.checker.contains_uppercase("njkshkhgiuo8"))
+
+
+    def test_contains_lowercase(self):
+        self.assertTrue(self.checker.contains_lowercase("nkshHIo0489"))
+
+    def test_no_lowercase(self):
+        self.assertFalse(self.checker.contains_lowercase("SNJGY6589I00Y"))
+
+
+    def test_contains_digit(self):
+        self.assertTrue(self.checker.contains_digit("njguoytr789u"))
+
+    def test_no_digit(self):
+        self.assertFalse(self.checker.contains_digit("nncjhgyuUHHG"))
+
+
+    def test_contains_special_character(self):
+        self.assertTrue(self.checker.contains_special_character("jgyuskl*?"))
+
+    def test_no_special_character(self):
+        self.assertFalse(self.checker.contains_special_character("bjgiyutFFFIUK2"))
+
+    def test_valid_password(self):
+        self.assertTrue(self.checker.is_valid_password("nkshHIo0489*"))
+        self.assertTrue(self.checker.is_valid_password("hope_Mabu67"))
+        self.assertTrue(self.checker.is_valid_password("nncjhgyu88+UHHG"))
+
+    def test_invalid_password(self):
+        self.assertFalse(self.checker.is_valid_password("njguoytr789u"))
+        self.assertFalse(self.checker.is_valid_password("SNJGY6589I00Y"))
+        self.assertFalse(self.checker.is_valid_password("nvbhg"))
+
+
 if __name__ == "__main__":
     unittest.main()
