56078edd1640cbd336b64afe312fa73632824029
Hope Mabuza <londiwemabuza94@gmail.com>
2026-01-10 18:47:50 +0200

    added psuedo code for vault_cipher

---
+++ sentryvault/vault_cipher.py
@@ -0,0 +1,17 @@
+class Vault_Cipher:
+
+    def encrypt (self, plain_text, key):
+        #if the key is correct(matches the key of the user);
+            #for every char in plain_text:
+                #shift its index by 5 from the original order of alphabets
+                #make sure you divide by 26 so that it remains in the correct order
+        
+        #return cipher_text
+
+    def dencrypt (cipher_text, key):
+        #if the key is correct(matches the key of the user);
+        #for every char in plain_text:
+            #shift its index by 5 (backwards) from the original order of alphabets
+            #make sure you divide by 26 so that it remains in the correct order
+        
+        #return plain_text
